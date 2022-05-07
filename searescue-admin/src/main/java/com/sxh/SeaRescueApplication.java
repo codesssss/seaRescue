c04968319695d56ec081f885d54ea42eecd05020
codesssss <xuhang.shi@outlook.com>
2022-05-07 17:15:17 +0800

    rename files, and upgrade the dependencies

---
+++ searescue-admin/src/main/java/com/sxh/SeaRescueApplication.java
@@ -0,0 +1,21 @@
+package com.sxh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * 启动程序
+ * 
+ * @author sxh
+ */
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class SeaRescueApplication
+{
+    public static void main(String[] args)
+    {
+        // System.setProperty("spring.devtools.restart.enabled", "false");
+        SpringApplication.run(SeaRescueApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  系统启动成功   ლ(´ڡ`ლ)ﾞ  \n" );
+    }
+}
